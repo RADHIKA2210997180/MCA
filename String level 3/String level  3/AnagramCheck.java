7c56a27714cd24b7c54fc7edb933c984a2146215
RADHIKA <127485327+RADHIKA2210997180@users.noreply.github.com>
2025-09-15 23:07:29 +0530

    Update AnagramCheck.java

--- String level 3/String level  3/AnagramCheck.java
+++ String level 3/String level  3/AnagramCheck.java
@@ -2,24 +2,23 @@ import java.util.Scanner;
 
 public class AnagramCheck {
     
-    // Method to check if two strings are anagrams
+    
     public static boolean isAnagram(String text1, String text2) {
         if (text1.length() != text2.length()) return false;
 
         int[] freq1 = new int[256];
         int[] freq2 = new int[256];
 
-        // Count frequency for text1
         for (int i = 0; i < text1.length(); i++) {
             freq1[text1.charAt(i)]++;
         }
 
-        // Count frequency for text2
+        
         for (int i = 0; i < text2.length(); i++) {
             freq2[text2.charAt(i)]++;
         }
 
-        // Compare both arrays
+       
         for (int i = 0; i < 256; i++) {
             if (freq1[i] != freq2[i]) return false;
         }
@@ -42,3 +41,4 @@ public class AnagramCheck {
             System.out.println("The texts are NOT Anagrams.");
     }
 }
+
